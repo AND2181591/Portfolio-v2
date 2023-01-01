58ea58acab282d7756804b871f198c5c2a21d8e1
Andrew <acdevelops1@gmail.com>
2023-01-01 15:00:13 -0700

    Built the layout for the About page and added some responsive styling between that page and the navbar.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from "@angular/core";
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @NgModule({
     imports: [
         MatIconModule, 
-        MatButtonModule
+        MatButtonModule, 
+        MatSidenavModule
     ], 
     exports: [
         MatIconModule, 
-        MatButtonModule
+        MatButtonModule, 
+        MatSidenavModule
     ]
 })
 export class MaterialModule {}
