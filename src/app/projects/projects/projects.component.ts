18dc0c2aee646bf7dcc8af452f707acb66e5a1a4
Andrew <acdevelops1@gmail.com>
2023-01-20 00:05:19 -0700

    Added starting Project page styling and layout.

--- src/app/projects/projects/projects.component.ts
+++ src/app/projects/projects/projects.component.ts
@@ -1,9 +1,16 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'], 
+  animations: [
+    trigger('pageAppear', [
+      transition(':enter', [ style({ opacity: 0 }), animate('.5s', style({ opacity: 1 })) ]), 
+      transition( ':leave', [ style({ opacity: 1 }), animate('.5s', style({ opacity: 0 })) ])
+    ]), 
+  ]
 })
 export class ProjectsComponent {
 
