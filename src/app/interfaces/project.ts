aaa6359062669f4bb6132fd30b58243159678fa3
Andrew <acdevelops1@gmail.com>
2023-01-22 16:55:44 -0700

    Created the layout for the Projects page.

---
+++ src/app/interfaces/project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    name: string;
+    image: string;
+    description: string;
+    accomplishments: string[];
+    techUsed: string[];
+    gitHubLink: string;
+    deployLink: string;
+}
--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from './projects/projects.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '../material.module';
 
 const routes: Routes = [
   { path: '', component: ProjectsComponent }
@@ -13,7 +14,8 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule, 
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes), 
+    MaterialModule
   ]
 })
 export class ProjectsModule { }
